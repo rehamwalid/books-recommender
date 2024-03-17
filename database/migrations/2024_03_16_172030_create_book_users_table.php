b821e46ebf0bb67ef489d93de502a6204df27425
RehamWalid <reham@34ml.com>
2024-03-17 17:26:10 +0200

    Created required tables

--- database/migrations/2024_03_16_172030_create_book_users_table.php
+++ database/migrations/2024_03_16_172030_create_book_users_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('book_users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('mobile')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id');
+            $table->foreignId('book_id');
+            $table->integer('start_page');
+            $table->integer('end_page');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('book_users');
     }
 };
