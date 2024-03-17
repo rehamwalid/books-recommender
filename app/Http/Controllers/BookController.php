e54bb41d1d53f196fe747a645b7ac54fad13170d
RehamWalid <reham@34ml.com>
2024-03-17 17:27:16 +0200

    Created Book controller

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\BookResource;
+use App\Models\Book;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = QueryBuilder::for(Book::class)
+            ->defaultSort('-num_read_pages')
+            ->get();
+        return response()->json(BookResource::collection($books));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'book_id' => 'required|exists:books,id',
+            'start_page' => 'required|integer',
+            'end_page'  =>  'required|integer'
+        ]);
+
+        $book = Book::findOrFail($validatedData['book_id']);
+        $user = User::findOrFail($validatedData['user_id']);
+
+        $book->users()->attach([
+            $validatedData['user_id'] => ['start_page' => $validatedData['start_page'],'end_page' => $validatedData['end_page']]
+        ]);
+
+        //update num of read pages
+        $book->numOfPages();
+
+        //Send Sms
+        $user->sendSms($book);
+
+        return new BookResource($book);
+    }
+
+}
---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "book_id" => $this->id,
+            "book_name" => $this->name,
+            "num_of_read_pages" => $this->num_read_pages
+        ];
+    }
+}
