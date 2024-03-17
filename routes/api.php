8a8c66e67837866c73c3b209929079029c2408e5
RehamWalid <reham@34ml.com>
2024-03-17 17:27:38 +0200

    Added routes required

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('book', \App\Http\Controllers\BookController::class)
+        ->only('store', 'index');
+});
